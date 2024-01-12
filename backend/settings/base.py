8db025275554e002fedd7d0db14ad29329903fb2
DataRohit <rohit.vilas.ingole@gmail.com>
2024-01-13 00:43:53 +0530

    Throttle Rate Added - Production

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -119,3 +119,13 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
+# Set the throttling rates
+REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/hour", "user": "100/hour"},
+}
